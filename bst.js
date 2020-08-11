65839acfedf85e0b0f851462bf2e6677e8cde8c0
Maksymilian Barnaś <max.barnas@gmail.com>
2020-08-11 23:36:06 +0200

    Add simple BST creation/traversal and template for Merge Sort

---
+++ bst.js
@@ -0,0 +1,61 @@
+( function () {
+    const items = [ 1, 7, 5, 8, 4, 2, 6, 3, 9 ];
+    let btree;
+
+    function createNode( data, parent = null, left = null, right = null ) {
+        return {
+            data,
+            left,
+            right
+        };
+    }
+
+    function insertNode( tree, data, parent, updateClb ) {
+        if ( !tree ) {
+            const node = createNode( data, parent );
+            updateClb( node );
+            return;
+        }
+
+        if ( data < tree.data ) {
+            insertNode( tree.left, data, parent, ( n ) => {
+                tree.left = n;
+            }
+            );
+        } else {
+            insertNode( tree.right, data, parent, ( n ) => {
+                tree.right = n;
+            }
+            );
+        }
+    }
+
+    function traverse( tree, buffer ) {
+        if ( !tree ) {
+            return;
+        }
+        traverse( tree.left, buffer );
+        buffer.push( tree.data );
+        traverse( tree.right, buffer );
+    }
+
+
+
+
+    console.clear();
+
+    for ( var i = 0; i < items.length; i++ ) {
+        insertNode( btree, items[ i ], null, ( node ) => {
+            btree = node;
+        }
+        );
+    }
+
+    var buf = [];
+
+    console.log( 'tree', btree );
+    traverse( btree, buf );
+    console.log( buf );
+    console.log( 'Tree traversed' );
+}
+)();
---
+++ mergesort.js
@@ -0,0 +1,12 @@
+( function () {
+    const items = [ 3, 4, 5, 7, 3, 5, 2, 1 ];
+
+    function mergeSort( items, low, high ) {
+        /// tbd
+    }
+
+    function merge() {
+        /// tbd
+    }
+
+} )();
