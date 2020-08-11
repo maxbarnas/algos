da1fdd3679af9bcda218f4b72fde62b065f7590f
Maksymilian Barnaś <max.barnas@gmail.com>
2020-08-12 00:17:59 +0200

    Update Merge Sort

--- mergesort.js
+++ mergesort.js
@@ -1,12 +1,44 @@
 ( function () {
-    const items = [ 3, 4, 5, 7, 3, 5, 2, 1 ];
+    const _items = [ 3, 2, 1, 4];
 
     function mergeSort( items, low, high ) {
-        /// tbd
+        if ( low < high ) {
+            const middle = Math.floor( ( low + high ) / 2 );
+            mergeSort( items, low, middle );
+            mergeSort( items, middle + 1, high );
+            merge( items, low, middle, high );
+        }
     }
 
-    function merge() {
-        /// tbd
+    function merge( items, low, middle, high ) {
+        const buffer1 = [], buffer2 = [];
+        var i;
+        
+        for ( i = low; i <= middle; i++ ) {
+            buffer1.push( items[ i ] );
+        }
+
+        for ( i = middle + 1; i <= high; i++ ) {
+            buffer2.push( items[ i ] );
+        }
+
+        i = low; // start from first item of the entire list
+
+        while ( buffer1.length && buffer2.length ) {
+            // Check which left-most element of both lists is the smallest (or equal)
+            if ( buffer1[ 0 ] <= buffer2[ 0 ] ) {
+                items[ i++ ] = buffer1.shift();
+            } else {
+                items[ i++ ] = buffer2.shift();
+            }
+        }
+
+        while ( buffer1.length ) { items[ i++ ] = buffer1.shift(); }
+        while ( buffer2.length ) { items[ i++ ] = buffer2.shift(); }
     }
 
+    console.clear();
+    console.log( _items );
+    mergeSort( _items, 0, _items.length - 1 );
+    console.log( _items );
 } )();
